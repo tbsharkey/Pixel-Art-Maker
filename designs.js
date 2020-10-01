7b5285e227005285abe1dbe0c455cd24304230aa
Tom <tbsharkey@gmail.com>
2020-09-30 18:40:02 -0700

    Added JS content

--- designs.js
+++ designs.js
@@ -1,10 +1,29 @@
-// Select color input
-// Select size input
+"use strict";
 
-// When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit( event => {
+    event.preventDefault();
+    let width = $('#inputWidth').val();
+    let height = $('#inputHeight').val();
+    $('#pixelCanvas').html(''); //clear
+    makeGrid(height, width);
+    addColor();
+});
 
-function makeGrid() {
+function makeGrid(height, width) {
+    for(let i = 0; i < height; i++) {
+      $('#pixelCanvas').append('<tr></tr>');
+    };
 
-// Your code goes here!
+    for(let i = 0; i < width; i++) {
+       $('tr').append('<td></td>');
+    };
+};
 
-}
+function addColor() {
+  $('td').click( event => {
+      let color = $('#colorPicker').val();
+      $(event.currentTarget).css("background-color", color)
+  });
+};
+
+
